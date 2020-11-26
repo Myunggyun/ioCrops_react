137401aefcd5197b7d3b3d21877a47da41c8e146
Son Myunggyun <sonmg90@gmail.com>
2020-11-26 15:15:17 +0900

    data 확인 작업내용 -data 확인완료 -data 한개 변수로 합침 -data 값 NaN으로 되어있음

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import data from './data/data.json'
+import data1 from './data/data-1.json'
+
+type JsonData = {
+    time:string;
+    EC_slab1:number | string;
+    EC_slab2:number | string;
+    EC_drain_PC:number | string;
+    WC_slab1:number | string;
+    WC_slab2:number | string;
+    CO2air:number | string;
+    HumDef:number | string;
+    Rhair:number | string;
+    Tair:number | string;
+    EnScr:number | string;
+    BlackScr:number | string;
+    PipeGrow:number | string;
+    PipeLow:number | string;
+    Iglob:number | string;
+    RadSum:number | string;
+    Tout:number | string;
+}[]
 
 function App() {
+  const [datas, setDatas] = useState<JsonData>([])
+
+  useEffect(()=>{
+    setDatas((prev)=>prev.concat(data.dataset, data1.dataset))
+  }, [])
+
   return (
     <div >hello world</div>
   );
